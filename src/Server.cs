d035bd1b95d5ba3d02cc4ec7a6f9c497fca8ecf6
Its0mar <omarjadallah24@gmail.com>
2026-04-04 08:34:31 +0300

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -8,5 +8,13 @@ Console.WriteLine("Logs from your program will appear here!");
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
 Socket client = server.AcceptSocket(); // wait for client
-client.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
+
+while (true)
+{
+    var buffer = new byte[1024];
+    int bytesRead = client.Receive(buffer);
+    if (bytesRead == 0) break;
+    client.Send(Encoding.UTF8.GetBytes("+PONG\r\n"));
+    
+}
 
